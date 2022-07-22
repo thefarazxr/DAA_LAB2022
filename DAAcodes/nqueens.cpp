c2fbec92016ea01521aa7ee282f8425a35ccb42a
thefarazxr <farazuddinmohammed05@gmail.com>
2022-07-22 18:25:35 +0530

    removed gdb comments

--- DAAcodes/nqueens.cpp
+++ DAAcodes/nqueens.cpp
@@ -4,15 +4,14 @@ COMPARING (j,x[j]) <==> (k,i)
 *******************************************************************************/
 #include <iostream>
 #include <math.h>
-
 using namespace std;
-int x[30],count=0;
+int x[30],count1=0;
 
 void printSol(int n)
 {
     int i,j;
-    count++;
-    cout<<"\nSolution #"<<count<<endl;
+    count1++;
+    cout<<"\nSolution #"<<count1<<endl;
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=n;j++)
@@ -63,7 +62,7 @@ int main()
     cin>>n;
     
     queen(1,n);
-    cout<<"\nTotal solutionas="<<count<<endl;
+    cout<<"\nTotal solutionas="<<count1<<endl;
 
     return 0;
 }
